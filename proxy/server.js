d790f7c222bd0d53630e6d5a34dda03166c3c3a1
Iris <irislam94@gmail.com>
2019-01-05 15:41:22 -0800

    Update dependencies

--- proxy/server.js
+++ proxy/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => console.log(`server running at: http://localhost:${port}`));
